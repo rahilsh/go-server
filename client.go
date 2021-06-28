53c0f07c250f61e271ab88d78ad5a7f9fa9253d3
Rahil S <rahilrshk@gmail.com>
2021-06-28 08:20:00 +0530

    Added client

---
+++ client.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	response, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	print("Response: ")
+	print(string(responseData))
+}
